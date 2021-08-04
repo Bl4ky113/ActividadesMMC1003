f0f429967b80f014933e8ddf2abe61b689c53f6e
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-08-04 08:48:31 -0500

    Agregado calcAceleration y Growth_arr a Plant

--- src/js/crecimiento_planta.js
+++ src/js/crecimiento_planta.js
@@ -18,8 +18,9 @@ const UNITS = {
 
 var plant = {
   total_size: 0,
-  sizes_arr: [],
   growth_rate: 0,
+  sizes_arr: [],
+  growth_arr: [],
   velocity_growth: [],
   aceleration_growth: []
 };
@@ -31,22 +32,26 @@ window.onload = () => {
   while (plant.growth_rate === 0) { plant.growth_rate = Math.floor(Math.random() * MAX_GROWTH_PER_DAY); }
   output_growth_rate.innerHTML += `${plant.growth_rate}${UNITS.SIZE} (aprox).`;
   
-  calcSize(plant, plant.total_size, plant.sizes_arr, plant.growth_rate, MAX_DAYS);
+  calcSize(plant, plant.total_size, plant.sizes_arr, plant.growth_arr, plant.growth_rate, MAX_DAYS);
   calcVelocity(plant, plant.sizes_arr, DAYS_COUNTING, MAX_DAYS);
+  calcAceleration(plant, plant.velocity_growth, DAYS_COUNTING);
   console.log(plant);
 }
 
-function calcSize (obj, total_size, sizes_array, grow_rate, days_num = 10) {
+function calcSize (obj, total_size, sizes_array, growth_arr, grow_rate, days_num = 10) {
   let random_growth = 0;
   let add_or_subtract = 0;
+  let total_growth = 0;
   for (let i = 0; i < days_num; i += 1) {
     random_growth = (Math.floor(Math.random() * 10) / 10);
     add_or_subtract = Math.floor(Math.random()) >= 1;
     if (add_or_subtract) {
-      total_size += grow_rate + random_growth;
+      total_growth = grow_rate + random_growth;
     } else {
-      total_size += grow_rate - random_growth;
+      total_growth = grow_rate - random_growth;
     }
+    total_size += total_growth;
+    growth_arr.push(total_growth);
     sizes_array.push(total_size);
   }
   obj.total_size = total_size;
@@ -64,3 +69,10 @@ function calcVelocity (obj, sizes_arr, days_counting = 1, days_num = 10) {
   }
 }
 
+function calcAceleration (obj, velocity_arr, days_counting = 1) {
+  let aceleration_growth = 0;
+  for (let i = 0; i < velocity_arr.length; i += 1) {
+    aceleration_growth = velocity_arr[i] / days_counting;
+    obj.aceleration_growth.push(aceleration_growth);
+  }
+}
