72c1ea75a264f10939ee33d25e02c3489bc4c04b
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-08-04 08:25:01 -0500

    Agregado calcVelocity

--- src/js/crecimiento_planta.js
+++ src/js/crecimiento_planta.js
@@ -9,6 +9,7 @@ let output_growth_rate = get.id("growthrate_output");
 
 const MAX_DAYS = 42;
 const MAX_GROWTH_PER_DAY = 5;
+const DAYS_COUNTING = 3;
 const UNITS = {
   SIZE: "cm",
   VELOCITY: "<sup>cm</sup>/<sub>día</sub>",
@@ -18,22 +19,27 @@ const UNITS = {
 var plant = {
   total_size: 0,
   sizes_arr: [],
-  growth_rate: 0
+  growth_rate: 0,
+  velocity_growth: [],
+  aceleration_growth: []
 };
 
 
 // Set a random number for the Plant's Growth rate
 window.onload = () => {
   plant.growth_rate = Math.floor(Math.random() * MAX_GROWTH_PER_DAY);
-  output_growth_rate.innerHTML += `${plant.growth_rate}cm (aprox).`;
+  while (plant.growth_rate === 0) { plant.growth_rate = Math.floor(Math.random() * MAX_GROWTH_PER_DAY); }
+  output_growth_rate.innerHTML += `${plant.growth_rate}${UNITS.SIZE} (aprox).`;
   
-  calcSize(plant.total_size, plant.sizes_arr, plant.growth_rate, MAX_DAYS);
+  calcSize(plant, plant.total_size, plant.sizes_arr, plant.growth_rate, MAX_DAYS);
+  calcVelocity(plant, plant.sizes_arr, DAYS_COUNTING, MAX_DAYS);
+  console.log(plant);
 }
 
-function calcSize (total_size, sizes_array, grow_rate, time_num = 10) {
+function calcSize (obj, total_size, sizes_array, grow_rate, days_num = 10) {
   let random_growth = 0;
   let add_or_subtract = 0;
-  for (let i = 0; i < time_num; i += 1) {
+  for (let i = 0; i < days_num; i += 1) {
     random_growth = (Math.floor(Math.random() * 10) / 10);
     add_or_subtract = Math.floor(Math.random()) >= 1;
     if (add_or_subtract) {
@@ -43,6 +49,18 @@ function calcSize (total_size, sizes_array, grow_rate, time_num = 10) {
     }
     sizes_array.push(total_size);
   }
-  plant.total_size = total_size;
+  obj.total_size = total_size;
+}
+
+function calcVelocity (obj, sizes_arr, days_counting = 1, days_num = 10) {
+  let velocity_growth = 0;
+  for (let i = days_counting - 1; i < days_num; i += days_counting) {
+    if ((i - days_counting) < 0) {
+      velocity_growth = sizes_arr[i] / days_counting;
+    } else {
+      velocity_growth = (sizes_arr[i] - sizes_arr[i - days_counting]) / days_counting;
+    }
+    obj.velocity_growth.push(velocity_growth);
+  }
 }
 
